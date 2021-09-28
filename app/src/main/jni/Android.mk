d5c4ecc43c66ed21eb106984d4672271780728d8
lamnguyen5464 <lamnguyen5464@gmail.com>
2021-09-28 23:00:21 +0700

    [jni] change properties modules

--- app/src/main/jni/Android.mk
+++ app/src/main/jni/Android.mk
@@ -8,32 +8,32 @@ include $(BUILD_SHARED_LIBRARY)
 include $(CLEAR_VARS)
 LOCAL_MODULE    := ConfigBase.h
 LOCAL_SRC_FILES := firebaseconfigs/ConfigBase/ConfigBase.cpp
-include $(BUILD_SHARED_LIBRARY)
+include $(BUILD_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE    := ConfigDevelopment.h
 LOCAL_SRC_FILES := firebaseconfigs/ConfigDevelopment/ConfigDevelopment.cpp
-include $(BUILD_SHARED_LIBRARY)
+include $(BUILD_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE    := ConfigStaging.h
 LOCAL_SRC_FILES := firebaseconfigs/ConfigStaging/ConfigStaging.cpp
-include $(BUILD_SHARED_LIBRARY)
+include $(BUILD_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE    := ConfigProduction.h
 LOCAL_SRC_FILES := firebaseconfigs/ConfigProduction/ConfigProduction.cpp
-include $(BUILD_SHARED_LIBRARY)
+include $(BUILD_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE    := FirebaseConfigFactory.h
 LOCAL_SRC_FILES := firebaseconfigs/FirebaseConfigFactory/FirebaseConfigFactory.cpp
-include $(BUILD_SHARED_LIBRARY)
+include $(BUILD_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE    := FirebaseConfigInstance.h
 LOCAL_SRC_FILES := firebaseconfigs/FirebaseConfigInstance/FirebaseConfigInstance.cpp
-include $(BUILD_SHARED_LIBRARY)
+include $(BUILD_STATIC_LIBRARY)
 
 
 
